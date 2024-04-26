924827c1c6ad570d671f63b564eb2e33368f5b03
Sami Islam <islam.samipkr@gmail.com>
2024-04-26 18:18:54 -0400

    Update storage_account_creation.tf

--- storage_account_creation.tf
+++ storage_account_creation.tf
@@ -1,12 +1,12 @@
 #This is an Azure Montreal College Information Technology - Lab Session
 
 resource "azurerm_resource_group" "azureresourcegroup" {
-  name     = "beststorageaccountworldbest"
+  name     = "MCIT_Lab_session"
   location = "Canada Central"
 }
 resource "azurerm_storage_account" "azurermmcitcountexample" {
   count=5
-  name                     = "${var.firstname}${var.lastname}storage${count.index}"
+  name                     = "${var.firstname}${var.lastname}makingitmorethan24charactersstorage${count.index}"
   resource_group_name      = azurerm_resource_group.azureresourcegroup.name
   location                 = azurerm_resource_group.azureresourcegroup.location
   account_tier             = var.account_tier
