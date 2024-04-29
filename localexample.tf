ab522ba98e3bfa4cb1de2effefcff17725926a72
rofmina <119059458+rofmina@users.noreply.github.com>
2024-04-29 19:59:43 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -2,10 +2,10 @@ locals{
 servicename="abracadabra"
 forum="abracadabramcit"
 lengthsa-length(local.servicename)
-character_enemy_map (for index, character in local.characters: #Convert charact
+character_enemy_map(for index, character in local.characters: #Convert charact
 lengthforum-length(local.forum)
 winterlistOfSports=["icehockey", "snowboarding", "iceskating"]
-total_output ["150", "150", "150"]
-characters ["luke", "yoda", "darth"]
+total_output["150", "150", "150"]
+characters["luke", "yoda", "darth"]
 enemies destroyed [4252, 900, 20000056894]
 character > local.enemies_destroyed [index]
