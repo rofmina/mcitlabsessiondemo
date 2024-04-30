49b54e02d347a7056490ec0f7332b944705968ec
rofmina <119059458+rofmina@users.noreply.github.com>
2024-04-29 20:42:11 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -91,3 +91,8 @@ default=["soccer","basketball","badminton","tabletennis"]
 output "summersportslist"{
 value=[for summersport in var.summersports:summersport ]
 }
+
+total_output = ["150", "150", "150"]
+output "sumofthreenumbers"{
+  value=sum([for numberoutput in local.total_output: tonumber(numberoutput)])
+}
