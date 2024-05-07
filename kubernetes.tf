7bbefe972e03630ef53066bbba62b0c7d5454198
rofmina <119059458+rofmina@users.noreply.github.com>
2024-05-06 20:53:25 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -10,3 +10,24 @@ resource "azurerm_resource_group" "minaazureresourcegroup2" {
   name     = "Mina2_MCIT_resource_group"
   location = "UAE Central"
 }
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
