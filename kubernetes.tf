55d0e9d18d9a4c25e03870a878f51d9259adba5c
rofmina <119059458+rofmina@users.noreply.github.com>
2024-05-06 20:05:39 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -2,7 +2,7 @@
 locals{ 
   cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
 }
-resource "azurerm_resource_group" "azureresourcegroup" {
-  name     = "Mina_MCIT_resource_group"
+resource "azurerm_resource_group" "minaazureresourcegroup" {
+  name     = "MCIT_resource_group"
   location = "Canada Central"
 }
