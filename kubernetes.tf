d71123e6ef345b1823a00b7b5593bb7e2c7d31c8
rofmina <119059458+rofmina@users.noreply.github.com>
2024-05-08 19:10:23 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -29,5 +29,36 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
 
   tags = {
     Environment = "Production"
+
+
+
+
+
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  
+  name                = "1111"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+
+
+
+
+
+
+
   }
 }
