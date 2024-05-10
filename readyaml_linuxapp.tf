27ec8a29533a5efd9e6baddd99fedf462c9ed545
rofmina <119059458+rofmina@users.noreply.github.com>
2024-05-10 19:46:42 -0400

    Update readyaml_linuxapp.tf

--- readyaml_linuxapp.tf
+++ readyaml_linuxapp.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_web_app" "batcha06webapp" {
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   location            = azurerm_resource_group.azureresourcegroup.location
   service_plan_id     = each.value.id
-  enabled             = each.value.enabled
+  
 
   site_config {}
 }
